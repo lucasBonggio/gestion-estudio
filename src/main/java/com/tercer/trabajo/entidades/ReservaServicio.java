34a141f26e97fb4ef2d0a15d31945f54c29e50e0
Lucas Bonggio <lucasbonggio1903@gmail.com>
2025-07-09 20:54:27 -0300

    correción: corregido el nombre de la entidad ServicioReserva que no coincidía con la tabla reserva_servicios

--- src/main/java/com/tercer/trabajo/entidades/ReservaServicio.java
+++ src/main/java/com/tercer/trabajo/entidades/ReservaServicio.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ServicioReserva {
+public class ReservaServicio {
     
     private int idReserva;
     private int idServicio;
